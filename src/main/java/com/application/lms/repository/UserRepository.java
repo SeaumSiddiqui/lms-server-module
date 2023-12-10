9e2e15d0b2b90dde2116c07b5359cd86295223f5
Seaum Siddiqui <seaumsiddiqui@yahoo.com>
2023-12-11 02:56:49 +0600

    User Authentication

--- src/main/java/com/application/lms/repository/UserRepository.java
+++ src/main/java/com/application/lms/repository/UserRepository.java
@@ -3,10 +3,14 @@ package com.application.lms.repository;
 import com.application.lms.domain.User;
 import com.application.lms.domain.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
     List<User> findByRole(UserRole role);
+
 }
